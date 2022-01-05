c8329a05b112e0d1b524a97b87fa9cba4741c2a5
Ahn sun young <68502219+skditjsdud35@users.noreply.github.com>
2022-01-05 15:56:59 +0900

    Add files via upload

---
+++ BOJ15552.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15552 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		int[] arr = new int[num];
+		
+		for(int i=0;i<num;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+		}
+		br.close();
+		
+		for(int i=0;i<num;i++) {
+			bw.write(arr[i]+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
